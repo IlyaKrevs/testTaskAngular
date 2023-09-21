2dc8c6665bc494052ccfe3858b42ad8406cb6951
Krevs <m-u-r-z-a@mail.ru>
2023-09-21 13:41:21 +0500

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularTestTask';
+  constructor() {
+    this.myFunc()
+  }
+
+  title = 'blablabla';
+
+  myFunc() {
+    console.log(this.title)
+  }
+  
 }
